9c22918d30b38d4a78a07a592451c766ea1f1dee
eacosta <eacosta0411@gmail.com>
2025-12-30 14:21:02 -0500

    Refactor print_row to include the ability to have "O"

--- lib/tic_tac_toe_board.rb
+++ lib/tic_tac_toe_board.rb
@@ -1,32 +1,32 @@
 # frozen_string_literal: true
 
 # Printing board using only a method.
-def print_board(row1, row2, row3)
-  row1_out = []
-  row1.each do |cell|
-    if cell == 1
-      row1_out.push("[X]")
-    else
-      row1_out.push("[ ]")
-    end
-  end
-  row2_out = []
-  row2.each do |cell|
-    if cell == 1
-      row2_out.push("[X]")
-    else
-      row2_out.push("[ ]")
-    end
-  end
-  row3_out = []
-  row3.each do |cell|
-    if cell == 1
-      row3_out.push("[X]")
-    else
-      row3_out.push("[ ]")
-    end
-  end
-end
+# def print_board(row1, row2, row3)
+#   row1_out = []
+#   row1.each do |cell|
+#     if cell == 1
+#       row1_out.push("[X]")
+#     else
+#       row1_out.push("[ ]")
+#     end
+#   end
+#   row2_out = []
+#   row2.each do |cell|
+#     if cell == 1
+#       row2_out.push("[X]")
+#     else
+#       row2_out.push("[ ]")
+#     end
+#   end
+#   row3_out = []
+#   row3.each do |cell|
+#     if cell == 1
+#       row3_out.push("[X]")
+#     else
+#       row3_out.push("[ ]")
+#     end
+#   end
+# end
 
 # Alternate idea for printing the board using class and instance variables.
 class Row
@@ -38,10 +38,12 @@ class Row
 
   def print_row
 
-    if @cell == 1
-      "[X]"
+    if @cell == 0
+      '[X]'
+    elseif @cell == 1
+      '[O]'
     else
-      "[ ]"
+      '[ ]'
     end
   end
 
