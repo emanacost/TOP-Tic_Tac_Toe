06baff3c2aa9da4d2e498a26b09d5470265fecef
eacosta <eacosta0411@gmail.com>
2025-12-30 13:17:48 -0500

    Create a method that will be used to print the tic tac toe board

--- lib/tic_tac_toe_board.rb
+++ lib/tic_tac_toe_board.rb
@@ -1,1 +1,28 @@
 # frozen_string_literal: true
+
+def print_board(row1, row2, row3)
+  row1_out = []
+  row1.each do |cell|
+    if cell == 1
+      row1_out.push("[X]")
+    else
+      row1_out.push("[ ]")
+    end
+  end
+  row2_out = []
+  row2.each do |cell|
+    if cell == 1
+      row2_out.push("[X]")
+    else
+      row2_out.push("[ ]")
+    end
+  end
+  row3_out = []
+  row3.each do |cell|
+    if cell == 1
+      row3_out.push("[X]")
+    else
+      row3_out.push("[ ]")
+    end
+  end
+end
