f6c3fb63e42736a19f641225fe62946b1c886049
eacosta <eacosta0411@gmail.com>
2025-12-30 14:36:52 -0500

    Remove initial method and add comments to tic_tac_toe_board.rb

--- lib/tic_tac_toe_board.rb
+++ lib/tic_tac_toe_board.rb
@@ -1,34 +1,6 @@
 # frozen_string_literal: true
 
-# Printing board using only a method.
-# def print_board(row1, row2, row3)
-#   row1_out = []
-#   row1.each do |cell|
-#     if cell == 1
-#       row1_out.push("[X]")
-#     else
-#       row1_out.push("[ ]")
-#     end
-#   end
-#   row2_out = []
-#   row2.each do |cell|
-#     if cell == 1
-#       row2_out.push("[X]")
-#     else
-#       row2_out.push("[ ]")
-#     end
-#   end
-#   row3_out = []
-#   row3.each do |cell|
-#     if cell == 1
-#       row3_out.push("[X]")
-#     else
-#       row3_out.push("[ ]")
-#     end
-#   end
-# end
-
-# Alternate idea for printing the board using class and instance variables.
+# Class and instance variables for creating a row.
 class Row
   attr_accessor :cell
   
@@ -49,6 +21,7 @@ class Row
 
 end
 
+# Method that uses Row class to create a row of cells for the board.
 def print_board(row)
   row_out = []
   row.each do |cell|
