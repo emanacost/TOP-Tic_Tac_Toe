1e70e080013708838897206ec2338c19039c0549
eacosta <eacosta0411@gmail.com>
2025-12-31 11:15:46 -0500

    Begin an alternative approach to game_loop logic using Class

--- lib/tic_tac_toe_loop.rb
+++ lib/tic_tac_toe_loop.rb
@@ -12,11 +12,35 @@ def game_loop
   puts 'Welcome to Tic Tac Toe!'
   puts 'Press any key to start the game.'
   gets.chomp
-  b = 1
-  while b < 4
-    print_row(eval("row#{b}"))
-    b += 1
+  playing_game = true
+  while playing_game
+    b = 1
+    while b < 4
+      print_row(eval("row#{b}"))
+      b += 1
+    end
   end
 end
 
 game_loop
+
+# Alternative class-based approach to organize the game loop
+class GameLoop
+  def initialize(row1, row2, row3)
+    @row1 = row1
+    @row2 = row2
+    @row3 = row3
+  end
+
+  def print_board
+    b = 0
+    while b < 3
+      print_row do
+        print_row(@row1)
+        print_row(@row2)
+        print_row(@row3)
+      end
+      b += 1
+    end
+  end
+end
