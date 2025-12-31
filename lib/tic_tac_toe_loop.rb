9bf4c2d245b2909ad8475742f6b0413b581c70c8
eacosta <eacosta0411@gmail.com>
2025-12-31 10:23:03 -0500

    Add library file that will be the main loop of the game

---
+++ lib/tic_tac_toe_loop.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Main loop for the game
+def game_loop
+end
--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 require_relative 'lib/tic_tac_toe_board'
+require_relative 'lib/tic_tac_toe_loop'
