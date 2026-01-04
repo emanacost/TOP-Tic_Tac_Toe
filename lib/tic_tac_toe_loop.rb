b41acaba5d8328153657f7ac332b8e34cdd6889c
eacosta <eacosta0411@gmail.com>
2026-01-04 00:23:47 -0500

    Refactor player move logic and add board update functionality

--- lib/tic_tac_toe_loop.rb
+++ lib/tic_tac_toe_loop.rb
@@ -25,6 +25,10 @@ class GameLoop
     @row3 = row3
   end
 
+  def move_tracker(move)
+    @move = move
+  end
+
   def print_board
     b = 0
     while b < 3
@@ -38,12 +42,36 @@ class GameLoop
   def self.player_move
     # Logic to handle player moves and update the board state.
     puts 'Please enter a move. Format: row, column. 1-3 for both row and column.'
-    move = gets.chomp.split(',').map(&:to_i)
+    @move = gets.chomp.split(',').map(&:to_i)
     # Input handling and validation logic here.
-    if move.length == 2 || move[0].between?(1, 3) && move[1].between?(1, 3)
-      puts "Nice! You choose row: #{move[0]} on column: #{move[1]}"
+    if @move.length == 2 || @move[0].between?(1, 3) && @move[1].between?(1, 3)
+      puts "Nice! You choose row: #{@move[0]} on column: #{@move[1]}"
     else
       puts 'Invalid input. Please try again.'
+    end
+  end
+
+  def self.update_board
+    # Logic to update the board based on player moves
+    case @move
+    when [1, 1]
+      @row1[0] = 0
+    when [1, 2]
+      @row1[1] = 0
+    when [1, 3]
+      @row1[2] = 0
+    when [2, 1]
+      @row2[0] = 0
+    when [2, 2]
+      @row2[1] = 0
+    when [2, 3]
+      @row2[2] = 0
+    when [3, 1]
+      @row3[0] = 0
+    when [3, 2]
+      @row3[1] = 0
+    when [3, 3]
+      @row3[2] = 0
     end
   end
 end
